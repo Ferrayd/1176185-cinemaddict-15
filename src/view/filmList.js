00b996144da9bd5bb6fc2772d1ee72ab439587f8
Ferrayd <kunitskiisv@gmail.com>
2021-08-17 08:56:40 +0500

    Fitst Commit

---
+++ src/view/filmList.js
@@ -0,0 +1,16 @@
+export const createList = () => (
+    `<section class="films">
+    <section class="films-list">
+    <div class="films-list__container films-list__container-one">
+    </div>
+    </section>
+    <section class="films-list films-list--extra">
+    <div class="films-list__container films-list__container-two">
+    </div>
+    </section>
+    <section class="films-list films-list--extra">
+    <div class="films-list__container films-list__container-three">
+    </div>
+    </section>
+    </section>`
+);
---
+++ src/view/footer.js
@@ -0,0 +1,3 @@
+export const createFooterStatistic = () => (
+    `<p>130 291 movies inside</p>`
+);
